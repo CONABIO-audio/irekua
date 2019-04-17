9b7d16420e95bbd443c9257c9cbc09d1114d6ac8
mbsantiago <santiago.mbal@gmail.com>
2019-04-17 16:11:50 -0500

    selia

--- irekua/selia/views.py
+++ irekua/selia/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def collections(request):
+    try:
+        user = request.user
+    except AttributeError:
+        pass
+    pass
