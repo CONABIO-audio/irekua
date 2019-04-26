f8a6f70cd6b1c82f7ff25a295539cb157d886fbe
Omar Miranda <mr.fumble@gmail.com>
2019-04-26 18:36:29 -0500

    first cycle to upload photos
    next: multi files

--- irekua/selia/views/forms.py
+++ irekua/selia/views/forms.py
@@ -6,5 +6,5 @@ from database.models import Item
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('item_type', 'item_file', 'created_by', 'sampling_event_device'  # 'captured_on', 'licence',
+        fields = ('item_type', 'item_file', 'created_by', 'sampling_event_device', 'licence',
         )
--- irekua/selia/views/upload.py
+++ irekua/selia/views/upload.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 from django import forms
 
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from .forms import PhotoForm
+from database.models import Item
 
 class NameForm(forms.Form):
     file = forms.FileField()
@@ -21,14 +23,17 @@ def upload(request):
     return render(request, 'selia/upload.html', context)
 
 def photo_list(request):
-    return render(request, 'selia/photo_list.html')
+    photos = Item.objects.all()
+    return render(request, 'selia/photo_list.html', {
+        'photos': photos
+        })
 
 def upload_photo(request):
     if request.method == 'POST':
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid:
             form.save()
-            return redirect('photo_list')
+            return redirect('selia:photo_list')
     else:
         form = PhotoForm()
     return render(request, 'selia/upload_photo.html',{
