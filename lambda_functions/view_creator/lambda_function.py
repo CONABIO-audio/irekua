a298d005e0926f4d846bcdc7294708aea07d3468
mbsantiago <santiago.mbal@gmail.com>
2019-05-16 17:54:15 -0500

    Could compile PIL for AWS machines. But ...

--- lambda_functions/view_creator/lambda_function.py
+++ lambda_functions/view_creator/lambda_function.py
@@ -1,9 +1,9 @@
-import json
 import os
+import io
 import mimetypes
 from urllib import parse
+
 import boto3
-import io
 
 from views import create_view
 
@@ -15,21 +15,18 @@ BUCKET_NAME = 'irekua-test'
 def lambda_handler(event, context):
     filename = extract_filename(event)
     database_info = extract_metadata_info(filename)
-
     item_file = get_file(filename)
     create_view(item_file, database_info)
 
 
 def extract_filename(event):
-    object = event['Records'][0]['s3']['object']
-
-    return parse.unquote_plus(object['key'])
+    return parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
 
 
 def extract_metadata_info(filename):
     relative_path = os.path.relpath(filename, BASE_PATH)
     collection, sampling_event, device, basename = relative_path.split('/')
-    hash_string, ext = os.path.splitext(basename)
+    hash_string, _ = os.path.splitext(basename)
 
     item_type = get_item_type(filename)
 
@@ -52,8 +49,7 @@ def get_file(filename):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(BUCKET_NAME)
 
-    basename = os.path.basename(filename)
     item_file = io.BytesIO()
-
     bucket.download_fileobj(filename, item_file)
+
     return item_file
