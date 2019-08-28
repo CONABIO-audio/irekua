49c0c081ff159e3006e0808f4db61d3f52cb3e9f
mbsantiago <santiago.mbal@gmail.com>
2019-08-28 12:14:54 -0500

    styles

--- irekua/selia/views/annotations/annotator.py
+++ irekua/selia/views/annotations/annotator.py
@@ -1,23 +1,39 @@
+from django import forms
 from .base import SeliaAnnotationView
 from database.models import Annotation
+
+
+class BootstrapRadioSelect(forms.RadioSelect):
+    template_name = 'selia/widgets/bootstrap_radio.html'
+
+
+class AnnotationCreateForm(forms.ModelForm):
+    class Meta:
+        model = Annotation
+        fields = [
+            "annotation_tool",
+            "item",
+            "event_type",
+            "label",
+            "annotation_type",
+            "annotation",
+            "annotation_configuration",
+            "certainty",
+            "quality",
+            "commentaries"
+        ]
+        widgets = {
+            "certainty": BootstrapRadioSelect,
+            "quality": BootstrapRadioSelect
+        }
 
 
 class CollectionItemAnnotatorView(SeliaAnnotationView):
     template_name = 'selia/annotations/annotator.html'
     annotator_template = 'selia/components/annotators/image.html'
-    model = Annotation
     success_url = 'selia:item_annotations'
     mode = 'create'
 
-    fields = [
-        "annotation_tool",
-        "item",
-        "event_type",
-        "label",
-        "annotation_type",
-        "annotation",
-        "annotation_configuration",
-        "certainty",
-        "quality",
-        "commentaries"
-    ]
+    model = Annotation
+    form_class = AnnotationCreateForm
+
