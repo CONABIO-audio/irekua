4aeaffea519def19c967b55967bd4830e4097bb6
mbsantiago <santiago.mbal@gmail.com>
2019-09-03 23:24:39 -0500

    annotation type serializer fixed

--- irekua/selia/views/annotations/annotator.py
+++ irekua/selia/views/annotations/annotator.py
@@ -9,14 +9,23 @@ class CollectionItemAnnotatorView(SeliaAnnotationView):
     template_name = 'selia/annotations/annotator.html'
 
     def get_urls(self):
+        collection = self.item.sampling_event_device.sampling_event.collection
         return {
-            'item': reverse('rest-api:item-detail', args=['item_pk']),
-            'item_type': reverse('rest-api:itemtype-detail', args=[mark_safe('item_type_pk')]),
-            'terms_autocomplete': reverse('selia:term_autocomplete')
+            'terms_autocomplete': reverse('selia:term_autocomplete'),
+            'item': reverse(
+                'rest-api:item-detail',
+                args=['item_pk']),
+            'item_type': reverse(
+                'rest-api:itemtype-detail',
+                args=[mark_safe('item_type_pk')]),
+            'annotation_types': reverse(
+                'rest-api:collectiontype-annotation-types',
+                args=[collection.collection_type.pk])
         }
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['item'] = Item.objects.get(pk=self.kwargs['pk'])
+        self.item = Item.objects.get(pk=self.kwargs['pk'])
+        context['item'] = self.item
         context['urls'] = self.get_urls()
         return context
